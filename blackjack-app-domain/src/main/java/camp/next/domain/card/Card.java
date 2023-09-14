d3a77909020a1270fa74cc7f17414ead9509f5da
Denny-Ko <donghar@naver.com>
2023-09-14 18:29:10 +0900

    블랙잭
    [Fix] 중복된 카드를 분배해주는 버그 수정

--- blackjack-app-domain/src/main/java/camp/next/domain/card/Card.java
+++ blackjack-app-domain/src/main/java/camp/next/domain/card/Card.java
@@ -1,5 +1,7 @@
 package camp.next.domain.card;
 
+import java.util.Objects;
+
 public class Card {
     private final Denomination number;
     private final Suit suit;
@@ -15,6 +17,19 @@ public class Card {
 
     public boolean isAce() {
         return this.number == Denomination.ACE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return number == card.number && suit == card.suit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, suit);
     }
 
     @Override
